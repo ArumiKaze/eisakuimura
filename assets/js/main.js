975fa8837a20e8a95ea17395645d7f52a2b08122
ArumiKaze <33790933+ArumiKaze@users.noreply.github.com>
2021-09-05 19:59:07 -0700

    Fix more console errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -207,7 +207,7 @@
 
 var slideIndex = 1;
 showSlides(slideIndex);
-
+  
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -226,5 +226,5 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  if (slides.length > 0) {slides[slideIndex-1].style.display = "block";}
 }
